99fa11421f7fdbee7c45dfaf8d49b0c7b89a7061
OrderedSet <orderedset86@gmail.com>
2022-11-25 16:03:53 -0800

    Retool for new setup

--- xp_crafter/autocrafter.py
+++ xp_crafter/autocrafter.py
@@ -15,13 +15,14 @@ bl, crafting_tl, spacing, crafting_result = (
 )
 
 BUCKET_PATTERN = [
-    [1, 0, 1],
-    [0, 0, 0],
+    [0, 1, 1],
+    [0, 0, 1],
     [0, 0, 0]
 ]
-pg.PAUSE = 0.05
+pg.PAUSE = 0.03
 DRY_RUN = False
-CURRENT_LEVEL = 64 # This assumes the tank is already full
+# CURRENT_LEVEL = 30 # This assumes the tank is already full
+tank_buckets = 53
 
 # Set up: look against tank bottom edge flat against drain
 
@@ -29,16 +30,17 @@ bucket_tape = list(chain.from_iterable(BUCKET_PATTERN)) # Flatten list, we can j
 num_buckets = sum(bucket_tape)
 
 # https://www.wolframalpha.com/input/?i=sum+110+%2B+9+%28n+-+30%29&assumption=%7B%22C%22%2C+%22sum%22%7D+-%3E+%7B%22SumWord%22%7D
-if CURRENT_LEVEL <= 15:
-    available_xp = CURRENT_LEVEL**2 + 6 * CURRENT_LEVEL
-elif CURRENT_LEVEL <= 30:
-    available_xp = 5/2 * (CURRENT_LEVEL**2 - 15 * CURRENT_LEVEL) + 315
-else:
-    available_xp = 1/2 * (9 * CURRENT_LEVEL**2 - 311 * CURRENT_LEVEL) + 1440 + 496
+# if CURRENT_LEVEL <= 15:
+#     available_xp = CURRENT_LEVEL**2 + 6 * CURRENT_LEVEL
+# elif CURRENT_LEVEL <= 30:
+#     available_xp = 5/2 * (CURRENT_LEVEL**2 - 15 * CURRENT_LEVEL) + 315
+# else:
+#     available_xp = 1/2 * (9 * CURRENT_LEVEL**2 - 311 * CURRENT_LEVEL) + 1440 + 496
 
 # from https://github.com/InnovativeOnlineIndustries/Industrial-Foregoing/issues/137
 essence_rate = 14 # 1 xp = 20 essence
-available_essence = available_xp * essence_rate + 16000
+# available_essence = available_xp * essence_rate + 16000
+available_essence = tank_buckets * 1000
 first_run = True
 empty_bucket_counter = 9
 
@@ -61,9 +63,9 @@ while available_essence >= 9000:
                 pg.rightClick()
                 sleep(0.1)
 
-    # Scoot back from tank alignment
-    with pg.hold('s'):
-        sleep(0.1)
+    # Scoot forward into crafting table
+    with pg.hold('w'):
+        sleep(0.5)
     pg.rightClick()
     sleep(4) # Wait for crafting window to pop up
 
@@ -117,29 +119,31 @@ while available_essence >= 9000:
 
     pg.press('esc')
     # Refill tank
-    # Press against tank
-    with pg.hold('w'):
+    # Back up into tank
+    with pg.hold('s'):
         sleep(0.2)
+    
+    sleep(1)
 
-    if available_essence > 7000: # Some xp left in player, 16 - 9 = 7
-        # Jump
-        with pg.hold('space'):
-            sleep(0.1)
-        # Move forward onto drain
-        with pg.hold('w'):
-            sleep(0.3)
-        # Wait for xp drain (6 seconds to drain 9000 xp)
-        sleep(
-            min(
-                max(0, 6*(available_essence-7000)/9000), # Time to drain all player XP
-                6*empty_bucket_counter/9 # Time to drain enough to refill tank
-            )
-        )
-        # Scoot back into tank position
-        with pg.hold('s'):
-            sleep(0.2)
-        sleep(0.4) # Need to fall back to ground
-        with pg.hold('w'):
-            sleep(0.2)
+    # if available_essence > 7000: # Some xp left in player, 16 - 9 = 7
+    #     # Jump
+    #     with pg.hold('space'):
+    #         sleep(0.1)
+    #     # Move forward onto drain
+    #     with pg.hold('w'):
+    #         sleep(0.3)
+    #     # Wait for xp drain (6 seconds to drain 9000 xp)
+    #     sleep(
+    #         min(
+    #             max(0, 6*(available_essence-7000)/9000), # Time to drain all player XP
+    #             6*empty_bucket_counter/9 # Time to drain enough to refill tank
+    #         )
+    #     )
+    #     # Scoot back into tank position
+    #     with pg.hold('s'):
+    #         sleep(0.2)
+    #     sleep(0.4) # Need to fall back to ground
+    #     with pg.hold('w'):
+    #         sleep(0.2)
 
     first_run = False
